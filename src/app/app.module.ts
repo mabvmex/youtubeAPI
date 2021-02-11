3690a33cac00288f57771e42b19fc838f2477d1a
mabvmex <mabvmex@gmail.com>
2021-02-10 22:49:01 -0600

    Servicio para obtener videos desde YT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -12,7 +14,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
+  private youtubeURL = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyCparuGObDfIp3cjUYQe5zNlqRCHFrZGJ0';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
 
-  constructor() { }
+  constructor( private http: HttpClient) { 
+
+  }
+
+
+// playlistItems?part=snippet&key=AIzaSyCparuGObDfIp3cjUYQe5zNlqRCHFrZGJ0&playlistId=UUuaPTYj15JSkETGnEseaFFg&maxResults=3
+
+  getVideos() {
+    const url = `${this.youtubeURL}/playlistItems`;
+
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('key', this.apiKey)
+    .set('playlistId', this.playlist)
+    .set('maxResults', '10')
+
+    return this.http.get(url, { params });
+  }
+
 }
